b6c3c777c3dc33d803a0c00cdf2836d5072d0f96
Tanushree <161806262+2001tanushree@users.noreply.github.com>
2024-03-04 23:15:57 -1200

    Added index.html

---
+++ script.js
@@ -0,0 +1,54 @@
+const toggleMenu = () => {
+    const navigation = document.querySelector('.navbar');
+
+    const burgerMenu = document.querySelector('.menu_icon');
+    const src = burgerMenu.getAttribute('src');
+
+    const isBurger = src === 'images/ham.png';
+    const iconName = isBurger ?
+    'images/cross.png': 'images/ham.png';
+
+
+/* Animate On Scroll
+    * ------------------------------------------------------ */
+var ssAOS = function() {     
+    AOS.init( {
+        offset: 200,
+        duration: 600,
+        easing: 'ease-in-sine',
+        delay: 300,
+        once: true,
+        disable: 'mobile'
+    });
+};
+
+/* Preloader
+    * -------------------------------------------------- */
+var ssPreloader = function() {
+        
+    $("html").addClass('ss-preload');
+
+    $WIN.on('load', function() {
+
+        //force page scroll position to top at page refresh
+        $('html, body').animate({ scrollTop: 0 }, 'normal');
+
+        // will first fade out the loading animation 
+        $("#loader").fadeOut("slow", function() {
+            // will fade out the whole DIV that covers the website.
+            $("#preloader").delay(300).fadeOut("slow");
+        }); 
+        
+        // for hero content animations 
+        $("html").removeClass('ss-preload');
+        $("html").addClass('ss-loaded');
+    
+    });
+};
+
+
+    burgerMenu.setAttribute(
+        'src' , iconName
+    );
+
+    }
